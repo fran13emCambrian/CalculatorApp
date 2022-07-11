01212686ba57160a687b1708d8cc38155f5c97d4
Francisco <a00223609@mycambrian.ca>
2022-07-11 19:19:41 -0230

    #ProjectCreate

--- Calculator.Logic/Class1.cs
+++ Calculator.Logic/Class1.cs
@@ -4,6 +4,9 @@ namespace Calculator.Logic
 {
     public class Class1
     {
-
+        //Addition
+        public static double Addition(double num1, double num2)
+        {
+            return num1 + num2;        }
     }
 }
