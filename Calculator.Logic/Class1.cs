90990ee21f12242685d77ca1bb59d7d687f606ae
Francisco <a00223609@mycambrian.ca>
2022-07-11 19:25:45 -0230

    #CalculatorLogic

--- Calculator.Logic/Class1.cs
+++ Calculator.Logic/Class1.cs
@@ -4,9 +4,35 @@ namespace Calculator.Logic
 {
     public class Class1
     {
-        //Addition
-        public static double Addition(double num1, double num2)
-        {
-            return num1 + num2;        }
+    //Addition
+    public static double Addition(double num1, double num2)
+    {
+        return num1 + num2;        
     }
+    //Subtraction
+    public static double Subtraction(double num1, double num2)
+    {
+        return num1 - num2;
+    }
+
+    //Multiplication
+    public static double Multiplication(double num1, double num2)
+    {
+        return num1 * num2;
+    }
+
+    //Division
+    public static double Division(double num1, double num2)
+    {
+        if (num2 == 0)
+        {
+            return -1;
+        }
+        else
+        {
+            return num1 / num2;
+        }
+    }
+}
+
 }
